8a06874b435ba2bbedd8a59b12a502900aeb7214
fruxc <hammadansari17@gmail.com>
2020-01-14 15:57:36 +0530

    Added cart option

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    constructor(public actionSheetController: ActionSheetController) {}
 
-  constructor() {}
-
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Cart',
+      buttons: [{
+        text: 'Remove',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Add to cart',
+        icon: 'arrow-dropright-circle',
+        handler: () => {
+          console.log('Added to cart');
+        }
+      }, {
+        text: 'Add to wishlist',
+        icon: 'heart',
+        handler: () => {
+          console.log('Added to wishlist');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
